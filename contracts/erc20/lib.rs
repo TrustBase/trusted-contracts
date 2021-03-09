fd1c7157936d81622c3d06cbeeff054667e3ec3c
Ethan <vencnte@gmail.com>
2021-03-09 08:19:02 +0000

    Update ink dependency to 3.0.0-rc3

--- contracts/erc20/lib.rs
+++ contracts/erc20/lib.rs
@@ -1,15 +1,3 @@
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use ink_lang as ink;
@@ -546,9 +534,10 @@ mod erc20 {
         }
 
         #[inline]
-        fn encode_to<T: scale::Output>(&self, dest: &mut T) {
+        fn encode_to<T: scale::Output + ?Sized>(&self, dest: &mut T) {
             self.prefix.encode_to(dest);
             self.value.encode_to(dest);
         }
     }
 }
+
